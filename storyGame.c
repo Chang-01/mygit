3953108ede202ed634abd7467c68fe7b8ca30b33
Chang-01 <ltt100@naver.com>
2024-11-10 12:19:00 +0900

    storyGame.c

--- storyGame.c
+++ storyGame.c
@@ -3,6 +3,8 @@
 #include "insert.h"
 #include "room_1.h"
 #include "room_2.h"
+#include "hallway.h"
+#include "ending.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,13 +18,13 @@ int main(void) {
 
 
 
-    startStory();
+    startStory_1();
     do {
         system("cls");
         if (phase == 1) {
             system("cls");   break;
         }
-        printf("천장엔 전구들이 깜빡이며 빛을 비추고 있었다.\n\n1. 침대를 조사한다\n\n2. 책상을 조사한다.\n\n3. 문을 조사한다.\n\n4. 얻은 정보를 확인한다.\n\n0. 종료\n\n");
+        printf("천장엔 세개의 전구들이 깜빡이며 빛을 비추고 있었다.\n\n1. 침대를 조사한다\n\n2. 책상을 조사한다.\n\n3. 문을 조사한다.\n\n4. 얻은 정보를 확인한다.\n\n0. 종료\n\n");
         n = select();
 
         switch (n) {
@@ -39,8 +41,9 @@ int main(void) {
 
 
     if (phase == 1) {
+        startStory_2();
         do {
-            printf("이제 어디로 이동할까.\n\n1. 왼쪽으로 간다.\n\n2. 오른쪽으로 간다.\n\n3. 연구원에게 말을 건다.\n\n4. 얻은 정보를 확인한다.\n\n0. 종료\n\n");
+            printf("이제 어디로 이동할까.\n\n1. 왼쪽으로 간다.\n\n2. 오른쪽으로 간다.\n\n3. 연구원들에게 다가간다.\n\n4. 얻은 정보를 확인한다.\n\n0. 종료\n\n");
             n = select();
 
             switch (n) {
@@ -54,5 +57,24 @@ int main(void) {
         } while (n != 0);
 
         return 0;
+    }
+
+    if (phase == 2) {
+        startStory_3();
+        do {
+            printf("이제 어디로 이동할까.\n\n1. 정면으로 간다.\n\n2. 여자를 확인한다.\n\n3. 얻은 정보를 확인한다.\n\n0. 종료\n\n");
+            n = select();
+
+            switch (n) {
+            case 1: forward();              system("pause");	system("cls");		break;
+            case 2:              system("pause");	system("cls");		break;
+            case 3: obtain();            system("pause");	system("cls");		break;
+            case 0: printf("종료되었습니다.\n");		break;
+            default:	 printf("잘못입력하였습니다.\n\n"); system("pause");	system("cls");
+            }
+        } while (n != 0);
+
+        return 0;
+
     }
 }
