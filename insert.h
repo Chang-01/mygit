e854df5244e3b2bcd2d540f605719e914c3b0dcf
Chang-01 <ltt100@naver.com>
2024-11-13 16:58:01 +0900

    Update insert.h

--- insert.h
+++ insert.h
@@ -19,8 +19,8 @@ void obtain();
 
 
 
-
-extern int key_1, key_2, key_3, key_4, key_5, key_6;
+int delay = 10;
+extern int key_1, key_2, key_3, key_4, key_5, key_6, key_7, key_8;
 
 void error() {
 	printf("잘못 입력하였습니다.\n");
@@ -43,7 +43,7 @@ void obtain() {
 		printf("- 신문에서 신원을 감추고 도망다니며 고급정보들을 훔쳤다는 사람의 이름이 내 이름과 같았고 그가 특정장소에서 목격되었다고 적혀있었다.\n\n");
 	}
 	if (key_2 == 1) {
-		printf("- 흑백사진 속 젊은 여성이 내 어깨에 손을 올리고 있었다. 이 여성은 나와 가까운 사이였던 사람 같았다.\n\n");
+		printf("- 흑백사진 속 젊은 여성이 내 어깨에 손을 올리고 있었다. 이 여성과 내가 어떤 일을 하려고 했던 거 같은데...\n\n");
 	}
 	if (key_3 == 1) {
 		printf("-다이어리에서 얻은 메모이다. 실험 시작일 : 7월 23일 \n\n");
@@ -56,6 +56,12 @@ void obtain() {
 	}
 	if (key_6 == 1) {
 		printf("-소파에서 얻은 가운이다.\n\n");
+	}
+	if (key_7 == 1) {
+		printf("-실험 보고서에서 얻은 정보 : 실험에 참여했던 연구원인 나는 기억을 조작당했지만, 완전히 지워지지 않은 기억 덕에 연구소의 감시를 피해 진실을 폭로하기 위해 탈출을 결심한다.\n\n");
+	}
+	if (key_8 == 1) {
+		//여자를 구하기 위한 단서
 	}
 
 }
@@ -75,3 +81,4 @@ void slow_print(const char* str, int delay_ms) {
 	}
 	putchar('\n');  // 줄바꿈
 }
+
